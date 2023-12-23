885e719a56be5cdbb68a40300cb0d513e1bf8397
tergav17 <gctersteeg@gmail.com>
2023-12-23 14:05:13 -0600

    Resizable textarea

--- websim/simulator.js
+++ websim/simulator.js
@@ -1,5 +1,7 @@
 const diag_flow = document.getElementById("diagflow");
+const monitor_text = document.getElementById("monitortext");
 
+console.log(monitor_text);
 // Attempt to get the context for the daig flow
 if (diag_flow.getContext) {
 	var flow_ctx = diag_flow.getContext("2d");
@@ -7,9 +9,14 @@ if (diag_flow.getContext) {
 	alert("Canvas not supported!");
 }
 
-// Add listeners for key evens
+// Add listeners for events
 window.addEventListener('keyup',keyUpListener,false);
 window.addEventListener('keydown',keyDownListener,false); 
+window.addEventListener('resize', resizeCanvas, false);
+
+// Do initial drawing of canvas
+draw(true);
+
 
 function keyUpListener(e) {
 	let k = e.key.toLowerCase();
@@ -27,6 +34,25 @@ function keyDownListener(e) {
 	}
 }
 
-flow_ctx.scale(1.0, 2.0);
-//flow_ctx.setTransform(1, 0, 0, 1, 0, 0);
-flow_ctx.fillRect(0, 0, 10, 10);
+function resizeCanvas() {
+	draw(true);
+}
+
+function draw(doResize) {
+	
+	// Handle resizing
+	if (doResize) {
+		
+		//let r
+		
+		flow_ctx.setTransform(1, 0, 0, 1, 0, 0);
+		flow_ctx.scale(1.0, 1.0);
+		
+		flow_ctx.canvas.width = window.innerWidth * (2/3);
+		flow_ctx.canvas.height = 9 * flow_ctx.canvas.width / 16;
+		monitor_text.style.height = (flow_ctx.canvas.height) + "px";
+		
+	}
+	
+	flow_ctx.fillRect(0, 0, 10, 10);
+}
