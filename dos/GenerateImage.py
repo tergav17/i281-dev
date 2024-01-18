78328702f7c51a97d8e107b249e9e5405a5d0a11
tergav17 <gctersteeg@gmail.com>
2024-01-17 23:58:39 -0600

    Finished first complete version of GenerateImage.py

--- dos/GenerateImage.py
+++ dos/GenerateImage.py
@@ -32,25 +32,54 @@ import sys
 def main():
     print("i281 DOS/281 FS Generate V0.1")
     print("SD Group 24-14")
-    print("Updated Jan 16, 2024")
+    print("Updated Jan 17, 2024")
     
     # Read in the master boot record
     mbr_f = open(sys.argv[1], mode="rb")
     mbr = mbr_f.read()
     
     # Get file system parameters
-    allocated_start = 1 + 16 + int(sys.argv[3]);
-    allocated_end = (256 * 256) - int(sys.argv[2]);
+    record_count = int(sys.argv[3])
+    allocated_start = 1 + 16 + record_count
+    allocated_end = int(sys.argv[2])
     
     # Create image file
     image = open("dos281.img", mode="wb")
     
     # Output MBR to image file
-    for o in range(512):
-        image.write((mbr[o]).to_bytes(1, byteorder='big'));
+    for i in range(512):
+        image.write((mbr[i]).to_bytes(1, byteorder='big'));
         
     # Output the allocation bitmap
-    
+    # This isn't the best way to write this algorithm, but it is late
+    # and I am lazy
+    # I'll rewrite this someday (never)
+    b = 0
+    for i in range(256 * 256):
+        b = b >> 1
+        
+        # Check lower bound
+        if i < allocated_start:
+            b = b | 0x80
+            
+        # Check upper bound
+        if i >= allocated_end:
+            b = b | 0x80
+        
+        # Write if the byte if full
+        if i % 8 == 7:
+            image.write((b).to_bytes(1, byteorder='big'));
+            
+    # Finally, output the record blocks
+    record = [0] * 512
+    for i in range(record_count):
+        # Record ending state
+        if i == record_count - 1:
+            record[511] = 0xFF
+        else:
+            record[511] = 0x01
+
+        image.write(bytes(record))
 
     # Output the rest of the filesystem as empty
     # for o in range((512 * 256 * 256) - 512):
